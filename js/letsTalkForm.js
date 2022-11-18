95a661106bf28ad647420ebc9a2a74b5247ad553
idriskelani <idmega2000@gmail.com>
2022-11-18 03:00:40 +0100

    add lets talk to all page and modify loader

--- js/letsTalkForm.js
+++ js/letsTalkForm.js
@@ -7,11 +7,18 @@ function scrollTopFunction(position) {
 }
 
 let loaderFunction = document.getElementById('loader')
-function openLoader() {
-  loaderFunction.classList.add('spinner-display')
+
+function openLoader(spnnerClass = 'spinner-display', selector = 'body', loaderId = 'loader') {
+  let loaderFunction = document.getElementById(loaderId)
+  let getBody = document.querySelector(selector);
+  loaderFunction.classList.add(spnnerClass);
+  getBody.classList.add('body-cover');
 }
-function hideLoader() {
-  loaderFunction.classList.remove('spinner-display')
+function hideLoader(spnnerClass = 'spinner-display', selector = 'body', loaderId = 'loader') {
+  let loaderFunction = document.getElementById(loaderId)
+  let getBody = document.querySelector(selector);
+  loaderFunction.classList.remove(spnnerClass)
+  getBody.classList.remove('body-cover');
 }
 
 let popupFunction = document.getElementById('pop-up')
@@ -22,48 +29,62 @@ function closePopup() {
   popupFunction.classList.remove('open-popup')
 }
 
-const formEl=document.querySelector('.form')    
-formEl.addEventListener('submit', event => {
-  event.preventDefault()  
+const formEl = document.querySelector('.form')
+formEl && formEl.addEventListener('submit', event => {
+  event.preventDefault()
   const formData = new FormData(formEl)
   const data = Object.fromEntries(formData)
   openLoader()
   setTimeout(hideLoader, 5000)
-  // openPopup()
   scrollTopFunction(40)
-  // fetch('https://wyreng.xyz/socials/contact/', {
-  //   method: 'POST',
-  //   headers: {
-  //    'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify(data)
-  // }).then(res => res.json())
-  //   .then(data => {formEl.reset()
-  //     openPopup()
-  // })
-  //   .catch(error => console.log(error)); 
+  fetch('https://wyreng.xyz/socials/contact/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  }).then(res => {
+    hideLoader();
+    if (res.ok) {
+      return res.json();
+    }
+
+    throw new Error('Something went wrong');
+  })
+    .then(data => {
+      formEl.reset()
+      openPopup()
+    })
+    .catch(error =>alert('something went wrong while processing the request, please try again'));
 });
 
 const letsTalk = document.querySelector('.lets-talk-form')
 
-letsTalk.addEventListener('submit', event => {
+letsTalk && letsTalk.addEventListener('submit', event => {
+  console.log('this is the add event listener here oooo ', letsTalk)
   event.preventDefault()
+  const success = document.querySelector('.w-form-done');
+  const failure = document.querySelector('.w-form-fail');
   const formData = new FormData(letsTalk)
   const data = Object.fromEntries(formData)
-  openLoader()
-  setTimeout(hideLoader, 5000)
-  // openPopup()
-  scrollTopFunction(40)
-  // fetch('https://wyreng.xyz/socials/lets_talk/', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify(data)
-  // }).then(res => res.json())
-  //   .then(data => {
-  //     letsTalk.reset()
-  //     alert('Welcome! its always our pleasure to serve you')
-  //   })
-  //   .catch(error => console.log(error));
+  openLoader('spinner-display', '.footer__form-container', 'loader-talk')
+  fetch('https://wyreng.xyz/socials/lets_talk/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  }).then(res => {
+    hideLoader('spinner-display', '.footer__form-container', 'loader-talk')
+    if (res.ok) {
+      success.classList.add('spinner-display')
+      return res.json();
+    }
+    throw new Error('Something went wrong');
+  })
+    .then(data => {
+      
+      letsTalk.reset()
+    })
+    .catch(error => failure.classList.add('spinner-display'));
 });
